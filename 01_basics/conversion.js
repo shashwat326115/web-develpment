776452c0e1849f1b34f0a34118b6e4a9178e8bb5
shashwat326115 <shashwatajmer2006@gmail.com>
2025-03-12 19:54:41 +0000

    container config done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,11 @@
+let score = "33abc"
+let valu = Number(score)
+console.log(typeof score)
+console.log(typeof valu)
+console.log(valu)
+//
+let  logged = 1
+let booleanisLoggedin = Boolean(logged)
+let somenumber = 33
+let stringnumber =String(somenumber) 
+console.log(typeof stringnumber)
