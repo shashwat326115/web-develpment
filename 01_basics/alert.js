f2c98e40b8ee1a009282609e9ec75c89c4f7569a
shashwat326115 <shashwatajmer2006@gmail.com>
2025-03-12 19:38:19 +0000

    container config done

---
+++ 01_basics/alert.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all js code as newer version 
+alert("hello")
+
+// number => 2 to power53
+// bigint => true/false
+// null=> standalone value
+// undefined => value not assigned yet
+//symbol =. unique react
+// object
+
+console
---
+++ 01_basics/justification.js
@@ -0,0 +1,7 @@
+let account_js = 5433
+const account = "i am shashwat"
+let account_city = "jaipur"
+console.table([account_js,account,account_city])
+// dont use var 
+// majorly use let as keyword
+//
