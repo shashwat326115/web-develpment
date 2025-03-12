0b0f0d3b30d3538fbcf434196ff67e8ca3961dfc
shashwat326115 <shashwatajmer2006@gmail.com>
2025-03-12 20:15:01 +0000

    container config done

---
+++ 01_basics/operation.js
@@ -0,0 +1,18 @@
+let value = 3
+let neg = -value
+// console.log(neg)
+// console.log(2**3)
+// console.log("1"+2)
+// console.log("1"+1+2)
+// console.log(1+1+"2")
+// console.log(3+4*5%3)
+console.log(+true)
+console.log(+"")
+//*****************************conditionals */
+console.log(2>1)
+console.log("2">1)
+console.log(2>"1")
+console.log(null>0)
+console.log(null>=0)//predictability is vanished
+console.log(null==0)
+// in case of undefined in above 3 case it will give false
