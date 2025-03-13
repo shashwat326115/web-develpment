480c3af0b6d31733ee29acbd0229615f52931ea3
shashwat326115 <shashwatajmer2006@gmail.com>
2025-03-13 06:41:55 +0000

    container config done

---
+++ 01_basics/math.js
@@ -0,0 +1,28 @@
+// const balance =2345
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))//we will set limit to number to be up to two decimal place
+// const other = 456.987654
+// console.log(other.toPrecision(4));//it will
+// const other1 = 46.987654
+// console.log(other1.toPrecision(3));
+// const other2 = 456.987654
+// console.log(other2.toPrecision(3));//basically round off before decimal and precision starting from number
+// const other3 = 4561.987654
+// console.log(other3.toPrecision(3));
+const hundreds =1000000
+console.log(hundreds.toLocaleString('en-IN'));
+// basically converting international to indian 
+//****************************MATHS */
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(45.678));
+// console.log(Math.ceil(4.2));// one succesvie value
+// console.log(Math.floor(4.9));// original number before decimal;
+// console.log(Math.min(4,5,6,7,8,5,3,45));
+console.log(Math.random()*10+1)// give random value betwween 0 and 1 and hencing multiplying by 10
+const min = 10
+const max = 20
+
+
+
---
+++ 01_basics/memory.js
@@ -0,0 +1,21 @@
+// stack (primitive
+//heap (non primitive))
+//********************************************stringd */
+"hello"+"world"
+console.log("hello"+"shreya")
+// but now we use string interpolation 
+let your = "shasheat mishra"
+let roll_no = "326456"
+
+console.log(`my name is ${your} and my roll no is ${roll_no}`)
+let game = new String("pubg")
+console.log(game[1])
+console.log(game.__proto__);
+console.log(game.length)
+console.log(game.toUpperCase())
+console.log(game.charAt())
+const slicest = game.slice(-4,3)
+console.log(game.slice(0,3))
+console.log(slicest)
+const newstt = game.substring(-8,3)
+console.log(newstt)
